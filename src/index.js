2c6663dce5cf53cc3f1dfd02a78998c4882c08b6
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-03-03 21:43:41 +0200

    basic part done

--- src/index.js
+++ src/index.js
@@ -9,31 +9,34 @@ import axios from 'axios';
 
 const MY_API_KEY = '33995663-3283b38da6c47940fd5e67885'; // мій персональний ключ з pixabay
 const BASE_URL = 'https://pixabay.com/api/';
+let page = 1;
+let PHOTO_NAME ="";
 // Підключаємось до данних сторінки index.html
 const refs = {
     form: document.querySelector('#search-form'),
     input: document.querySelector('input'),
-    imageBox: document.querySelector('.image-box'),
+    gallery: document.querySelector('.gallery'),
+    btnLoadMore: document.querySelector('.load-more'), 
 };
-
-console.log(refs.formInput); // TEST
-console.log(refs.formSubmit); // TEST
-console.log(refs.imageBox); // TEST
-console.log(`test 1`);
 
 // -------------------------------------------------------------
 
+refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку load-more
+refs.btnLoadMore.addEventListener('click', loadMore); // Відслідковуваємо подію на кнопці load-more
+refs.form.addEventListener('submit', onFormSubmit); // Відслідковуваємо подію submit
 
-// Відслідковуваємо подію submit
-refs.form.addEventListener('submit', onFormSubmit);
+
 
 
 // ФУНКЦІЯ - перевіряємо наявність зображень
 function onFormSubmit (evt){
+    
     evt.preventDefault(); // відміна перезавантаження сторінки
-    const name = refs.input.value.trim(); // обрізання пробілів до і після слова
-    console.log(name);
-
+    const name = refs.input.value.trim(); // редагуємо текст, прибираємо пробіли
+    PHOTO_NAME = name;
+    refs.gallery.innerHTML = ''; // очищення попереднього вмісту галереї
+    page = 1;
+    
     // якщо слово пошука НЕ пуста строка то:
     if (name !== '') {
         pixabay(name); // отримати зображення
@@ -49,34 +52,100 @@ function onFormSubmit (evt){
     }
 
 
+
 };
 
 
 
 
-// отримання зображень з https://pixabay.com
+
+
+// ФУНКЦІЯ - отримання зображень з https://pixabay.com
 async function pixabay(name, page) {
-    console.log(`test 2`);
+    
     // параметри запиту на бекенд
-    // const options = {
-    //   params: {
-    //     key: MY_API_KEY, // мій персональний ключ з pixabay
-    //     q: name,
-    //     image_type: 'photo',
-    //     orientation: 'horizontal',
-    //     safesearch: 'true',
-    //     page: page,
-    //     per_page: 40,
-    //   },
-    // };
+    const options = {
+      params: {
+        key: MY_API_KEY, // мій персональний ключ з pixabay
+        q: name,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: page,
+        per_page: 40,
+      },
+    };
 
-    const imgSerch = "https://pixabay.com/api/?key=" + MY_API_KEY+"&q="+name;
-    $.getJSON(URL, function(data){
-    if (parseInt(data.totalHits) > 0){
-        console.log(imgSerch);
-    }else{
-        console.log('No hits');}
-    });
+    try {
+        // отримання відповіді-результату від бекенду
+        const response = await axios.get(BASE_URL, options);
+        console.log(`totalHits for "${name}" : ${response.data.total}`); // TEST
+        // сповіщення notiflix
+        // notification(
+        //   response.data.hits.length, // довжина всіх знайдених зображень
+        //   response.data.total // отримання кількості
+        // );
+    
+        createMarkup(response.data.hits); // рендер розмітки на сторінку
+      } catch (error) {
+        console.log(error);
+      }
+}
 
-    console.log(imgSerch);
-};
+// ФУНКЦІЯ - створення карток з зображеннями
+
+function createMarkup (stock){
+
+ const imgStock = stock.map( item => 
+    `<div class="photo-card">
+    <div class="photo">
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+    </div>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes:</b>
+        ${item.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${item.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${item.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${item.downloads}
+      </p>
+    </div>
+  </div>`)
+  .join(''); // сполучення рядків всіх об'єктів (всіх картинок)
+  
+  refs.gallery.insertAdjacentHTML('beforeend', imgStock); // вставлення розмітки на сторінку
+
+  // ПЕРЕВІРКА - на кнопку Load more 
+    if (stock.length === 40) {
+        refs.btnLoadMore.style.display = 'flex'; // показуємо кнопку Load more 
+    }else if (stock.length === 0){
+        refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку Load more 
+        return Notiflix.Notify.failure(
+            `Sorry, there are no images matching your search query. Please try again.`
+            ,{width:'350px', borderRadius: '10px', position: 'center-center',clickToClose: true, useIcon: false,}
+            );
+    }else if (stock.length > 1 || stock.length < 40 ){
+        refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку Load more 
+        return Notiflix.Notify.info(
+            `We're sorry, but you've reached the end of search results.`
+            ,{width:'350px', borderRadius: '10px', position: 'center-center',clickToClose: true, useIcon: false,}
+            );
+    }
+}
+
+
+// ФУНКЦІЯ - на кнопку Load more 
+function loadMore (){
+    refs.btnLoadMore.style.display = 'none'; // ТЗ - При повторному сабміті форми кнопка спочатку ховається, а після запиту знову відображається.
+    pixabay(PHOTO_NAME, page+=1);
+
+}
